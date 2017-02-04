f527e0e124c36dc18ffa356d5c4a4b5052819454
Steve Smodish <stevesmodish@gmail.com>
2017-02-04 17:54:49 -0500

    BEM/PostCSS style edits

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	autoprefixer = require('autoprefixer'),
 	cssvars = require('postcss-simple-vars'),
 	nested = require('postcss-nested'),
+	cssImport = require('postcss-import'),
 	watch = require('gulp-watch');
 
 gulp.task('default', function() {
@@ -15,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
