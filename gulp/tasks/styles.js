9a9002c7ce3b396b8216eed302f2d34add5a3dd7
Steve Smodish <stevesmodish@gmail.com>
2017-02-10 13:31:22 -0500

    edit js formatting a little bit

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,7 +1,7 @@
-var 	gulp 				= require('gulp'),
-		postcss 			= require('gulp-postcss'),
+var 	gulp 			= require('gulp'),
+		postcss 		= require('gulp-postcss'),
 		autoprefixer 	= require('autoprefixer'),
-		cssvars 			= require('postcss-simple-vars'),
+		cssvars 		= require('postcss-simple-vars'),
 		nested 			= require('postcss-nested'),
 		cssImport 		= require('postcss-import'),
 		mixins 			= require('postcss-mixins');
