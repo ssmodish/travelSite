97031e1357023f5befbb524bcc9eea8c6764f92e
Steve Smodish <stevesmodish@gmail.com>
2017-02-11 01:12:50 -0500

    using gulp to generate sprite sheet and corresponding css from individual svg files

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-	svgSprite = require('gulp-svg-sprite');
+	svgSprite = require('gulp-svg-sprite'),
+    rename = require('gulp-rename');
 
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
             render: {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -11,10 +13,24 @@ var config = {
             }
         }
     }
-}
+};
 
 gulp.task('createSprite', function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+   return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic' ,'copySpriteCSS']);
