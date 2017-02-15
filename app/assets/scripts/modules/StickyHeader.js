9824aba9575d2cc984081727fca9549e4cdf9c0c
Steve Smodish <stevesmodish@gmail.com>
2017-02-15 10:52:47 -0500

    Begin work on sticky header.

---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class StickyHeader {
+    constructor() {
+        this.siteHeader = $(".site-header");
+        this.headerTriggerElement = $(".large-hero__title");
+        this.createHeaderWaypoint();
+    }
+
+    createHeaderWaypoint() {
+        var that = this;
+        new Waypoint({
+            element: this.headerTriggerElement[0],
+            handler: function() {
+                that.siteHeader.addClass("site-header--dark");
+            }
+        });
+    }
+}
+
+export default StickyHeader;
